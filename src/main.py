8ad2153f82942d9e308e39af28f1409412d2c3b1
Kunio SAWATANI <fsawa@fathens.org>
2017-01-07 08:57:53 +0000

    remove と merge を実装

--- src/main.py
+++ src/main.py
@@ -8,19 +8,25 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+client = boto3.client('cognito-identity')
+
 def lambda_handler(event, context):
     if "add" in event:
         return add(event["add"])
     if "remove" in event:
         return remove(event["remove"])
+    if "merge" in event:
+        return merge(event["merge"])
     raise Exception("No request")
 
 def add(params):
     logger.info("Adding: " + str(params))
-    client = boto3.client('cognito-identity')
-    res = client.get_open_id_token_for_developer_identity(**params)
-    return res
-    
+    return client.get_open_id_token_for_developer_identity(**params)
+
 def remove(params):
     logger.info("Removing: " + str(params))
-    return "Removed"
+    return client.unlink_developer_identity(**params)
+
+def merge(params):
+    logger.info("Merging: " + str(params))
+    return client.merge_developer_identities(**params)
